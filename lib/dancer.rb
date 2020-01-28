43b0df6e4d762c365d8c95cdaae74099d12eec70
dwane richards <dwanerich@gmail.com>
2020-01-28 00:11:59 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,9 +4,7 @@ require_relative './fancy_dance.rb'
 
 class Dancer
     extend FancyDance::ClassMethods
-    include FancyDance::InstanceMethods
-
-  include Dance
+    include Dance, FancyDance::InstanceMethods
 
   attr_accessor :name
 
